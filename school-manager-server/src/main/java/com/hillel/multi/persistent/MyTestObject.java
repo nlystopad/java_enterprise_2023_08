728b6666e76167c79864628d03e6b889875d79aa
Nikita Lystopad <nikita12312123@gmail.com>
2023-08-26 19:42:00 +0300

    added MyTestObject and MyTestController, added Spring boot starter web dependencies

---
+++ school-manager-server/src/main/java/com/hillel/multi/persistent/MyTestObject.java
@@ -0,0 +1,34 @@
+package com.hillel.multi.persistent;
+
+import org.springframework.lang.NonNull;
+
+public class MyTestObject {
+    @NonNull
+    String name;
+    Integer length;
+    Integer width;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public void setLength(Integer length) {
+        this.length = length;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+}
