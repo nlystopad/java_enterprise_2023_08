9636ee05c1b7687b6756527ed97bfb69ca3b2624
Nikita Lystopad <nikita12312123@gmail.com>
2023-09-13 14:52:07 +0300

    added exceptions

---
+++ school-manager-server/src/main/java/com/hillel/multi/inftastructure/exceptions/AlreadyExistException.java
@@ -0,0 +1,18 @@
+package com.hillel.multi.inftastructure.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class AlreadyExistException extends RuntimeException {
+    public AlreadyExistException() {
+    }
+
+    public AlreadyExistException(String message) {
+        super(message);
+    }
+
+    public AlreadyExistException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ school-manager-server/src/main/java/com/hillel/multi/inftastructure/exceptions/ProblemDetails.java
@@ -0,0 +1,12 @@
+package com.hillel.multi.inftastructure.exceptions;
+
+import java.util.Date;
+
+public class ProblemDetails {
+
+    String description;
+    String errorType;
+    String exceptionMessage;
+    String rootCause;
+    Date timestamp;
+}
