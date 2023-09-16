e9e2007dbcc993b1eb8698bbc378697dc8a09c3e
Nikita Lystopad <nikita12312123@gmail.com>
2023-09-16 11:32:54 +0300

    added entity structure

--- school-manager-server/src/main/java/com/hillel/multi/persistent/entity/School.java
+++ school-manager-server/src/main/java/com/hillel/multi/persistent/entity/School.java
@@ -1,16 +1,33 @@
 package com.hillel.multi.persistent.entity;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
 @Getter
 @Setter
+@Entity
+@Table(name = "schools")
 public class School {
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "address")
+    private String address;
+
+    @OneToMany(mappedBy = "school")
     private List<Student> students;
+
+    @OneToMany(mappedBy = "school")
     private List<Teacher> teachers;
+
+    @OneToMany(mappedBy = "school")
     private List<ClassRoom> classrooms;
-    // todo: add more fields
+
+
 }
--- school-manager-server/src/main/java/com/hillel/multi/persistent/entity/Teacher.java
+++ school-manager-server/src/main/java/com/hillel/multi/persistent/entity/Teacher.java
@@ -1,5 +1,6 @@
 package com.hillel.multi.persistent.entity;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,13 +8,37 @@ import java.util.List;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "teachers")
 public class Teacher {
+    @Id
+    @GeneratedValue
     private Long id;
-    private School school;
-    private List<Student> students;
-    private TeacherRole role;
-    private ClassRoom classRoom;
-    // todo: add more fields
 
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "age")
+    private Integer age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "school_id")
+    private School school;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "teachers_students",
+            joinColumns = @JoinColumn(name = "teacher_id"),
+            inverseJoinColumns = @JoinColumn(name = "student_id"))
+    private List<Student> students;
+
+    @Enumerated(EnumType.STRING)
+    private TeacherRole role;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "class_room_id", referencedColumnName = "id")
+    private ClassRoom classRoom;
 
 }
